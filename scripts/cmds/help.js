eeda489d091ba7c27e777b6091d9e1cc07e6acb0
ranielhiyan184 <165879920+ranielhiyan184@users.noreply.github.com>
2024-07-03 13:32:43 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -1,100 +1,116 @@
-module.exports.config = {
-  name: 'help',
-  version: '1.0.0',
-  role: 0,
-  hasPrefix: true,
-  aliases: ['info'],
-  description: "Beginner's guide",
-  usage: "Help [page] or [command]",
-  credits: 'Develeoper',
+const commandInfoMap = {
+  ai: {
+    name: "ai",
+    description: "Ai Based on GPT-4",
+    guide: "-ai what is life?"
+    },
+  tempmail: {
+    name: "tempmail",
+    description: "Get Temporary Emails and it's Inbox messages",
+    guide: "-tempmail create\n-tempmail inbox <email>"
+  },
+  alldl: {
+    name: "alldl",
+    description: "download video content using link from Facebook, Instagram, Tiktok, Youtube, Twitter, and Spotify audio",
+    guide: "-alldl [link]"
+  },
+animagine: {
+    name: "animagine",
+    description: "makes an animated image based on your imagination",
+    guide: "-animagine cat with wings"
+    },
+  translate: {
+    name: "translate",
+    description: "Translate to any languages",
+    guide: "Reply to text you want to translate and type \n-translate <language>"
+  },
+  pinterest: {
+    name: "pinterest",
+    description: "Searches Images in Pinterest ",
+    guide: "-pinterest cat -10"
+  },
+  dalle: {
+    name: "dalle",
+    description: "make images through texts",
+    guide: "-dalle cat in a hoodie"
+  },
+  remini: {
+    name: "remini",
+    description: "enhances your image to lessen the blur",
+    guide: "reply to image and type -remini"
+  },
+  lyrics: {
+    name: "lyrics",
+    description: "Fetches lyrics of a song",
+    guide: "-lyrics perfect by ed sheeran"
+  },
+  help: {
+    name: "help",
+    description: "View all commands",
+    guide: "-help\n-help <command name>"
+  },
+  prefix: {
+    name: "prefix",
+    description: "view some commands and shows bot's prefix",
+    guide: "prefix"
+  },
+  uptime: {
+    name: "uptime",
+    description: "See how long the bot has been running.",
+    guide: "-uptime"
+  },
+  unsend: {
+    name: "unsend",
+    description: "deletes bot messages",
+    guide: "reply to bot message and type -unsend"
+  },
 };
-module.exports.run = async function({
-  api,
-  event,
-  enableCommands,
-  args,
-  Utils,
-  prefix
-}) {
-  const input = args.join(' ');
-  try {
-    const eventCommands = enableCommands[1].handleEvent;
-    const commands = enableCommands[0].commands;
-    if (!input) {
-      const pages = 20;
-      let page = 1;
-      let start = (page - 1) * pages;
-      let end = start + pages;
-      let helpMessage = `Command List:\n\n`;
-      for (let i = start; i < Math.min(end, commands.length); i++) {
-        helpMessage += `\t${i + 1}. 「 ${prefix}${commands[i]} 」\n`;
-      }
-      helpMessage += '\nEvent List:\n\n';
-      eventCommands.forEach((eventCommand, index) => {
-        helpMessage += `\t${index + 1}. 「 ${prefix}${eventCommand} 」\n`;
-      });
-      helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. To view the next page, type '${prefix}help page number'. To view information about a specific command, type '${prefix}help command name'.`;
-      api.sendMessage(helpMessage, event.threadID, event.messageID);
-    } else if (!isNaN(input)) {
-      const page = parseInt(input);
-      const pages = 20;
-      let start = (page - 1) * pages;
-      let end = start + pages;
-      let helpMessage = `Command List:\n\n`;
-      for (let i = start; i < Math.min(end, commands.length); i++) {
-        helpMessage += `\t${i + 1}. 「 ${prefix}${commands[i]} 」\n`;
-      }
-      helpMessage += '\nEvent List:\n\n';
-      eventCommands.forEach((eventCommand, index) => {
-        helpMessage += `\t${index + 1}. 「 ${prefix}${eventCommand} 」\n`;
-      });
-      helpMessage += `\nPage ${page} of ${Math.ceil(commands.length / pages)}`;
-      api.sendMessage(helpMessage, event.threadID, event.messageID);
-    } else {
-      const command = [...Utils.handleEvent, ...Utils.commands].find(([key]) => key.includes(input?.toLowerCase()))?.[1];
-      if (command) {
-        const {
-          name,
-          version,
-          role,
-          aliases = [],
-          description,
-          usage,
-          credits,
-          cooldown,
-          hasPrefix
-        } = command;
-        const roleMessage = role !== undefined ? (role === 0 ? '➛ Permission: user' : (role === 1 ? '➛ Permission: admin' : (role === 2 ? '➛ Permission: thread Admin' : (role === 3 ? '➛ Permission: super Admin' : '')))) : '';
-        const aliasesMessage = aliases.length ? `➛ Aliases: ${aliases.join(', ')}\n` : '';
-        const descriptionMessage = description ? `Description: ${description}\n` : '';
-        const usageMessage = usage ? `➛ Usage: ${usage}\n` : '';
-        const creditsMessage = credits ? `➛ Credits: ${credits}\n` : '';
-        const versionMessage = version ? `➛ Version: ${version}\n` : '';
-        const cooldownMessage = cooldown ? `➛ Cooldown: ${cooldown} second(s)\n` : '';
-        const message = ` 「 Command 」\n\n➛ Name: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
-        api.sendMessage(message, event.threadID, event.messageID);
-      } else {
-        api.sendMessage('Command not found.', event.threadID, event.messageID);
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-module.exports.handleEvent = async function({
-  api,
-  event,
-  prefix
-}) {
-  const {
-    threadID,
-    messageID,
-    body
-  } = event;
-  const message = prefix ? 'This is my prefix: ' + prefix : "Sorry i don't have prefix";
-  if (body?.toLowerCase().startsWith('prefix')) {
-    api.sendMessage(message, threadID, messageID);
-  }
-}
 
-]
+module.exports = {
+  config: {
+    name: "help",
+    aliases: ["help"],
+    version: 1.0,
+    author: "LiANE&Coffee",
+    shortDescription: { en: "View all commands" },
+    category: "members",
+  },
+  onStart: async function({ message, args }) {
+    if (args[0]) {
+      const command = args[0].toLowerCase();
+      if (commandInfoMap[command]) {
+        const { name, description, guide } = commandInfoMap[command];
+        const response = `━━━━━━━━━━━━━━━━\n𝙲𝚘𝚖𝚖𝚊𝚗𝚍 𝙽𝚊𝚖𝚎: ${name}\n𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚝𝚒𝚘𝚗: ${description}\n𝙶𝚞𝚒𝚍𝚎: ${guide}\n━━━━━━━━━━━━━━━━`;
+        return message.reply(response);
+      } else {
+        return message.reply("Command not found.");
+      }
+    } else {
+      const commandsList = `━━━━━━━━━━━━━━━━
+𝙰𝚟𝚊𝚒𝚕𝚊𝚋𝚕𝚎 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜:
+╭─╼━━━━━━━━╾─╮
+│ - AI
+│ - Translate 
+│ - Animagine 
+│ - Dalle
+│ - Pinterest
+│ - Remini
+│ - Lyrics
+│ - Alldl
+│ - Help
+│ - Prefix
+│ - Tempmail
+│ - Unsend
+│ - Uptime
+╰─━━━━━━━━━╾─╯
+-𝚑𝚎𝚕𝚙 <𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚗𝚊𝚖𝚎>
+𝚃𝚘 𝚜𝚎𝚎 𝚑𝚘𝚠 𝚝𝚘 𝚞𝚜𝚎
+𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜.
+
+Example: -help ai
+━━━━━━━━━━━━━━━━`;
+
+      return message.reply(commandsList);
+    }
+  }
+};
